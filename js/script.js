d58b9ced58b46f5f4dfa466e1f9e7a5a9385334d
Hernan <hernanchoquedev@gmail.com>
2021-10-15 22:40:46 -0300

    refactory code and add animation - HTML - CSS - JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-const menuIcon = document.querySelector(".nav-page__menu-image");
-const menu = document.querySelector(".nav-page__list");
+const menuIcon = document.querySelector(".menu-layout__image");
+const menu = document.querySelector(".menu-layout__list");
 
 
 menuIcon.addEventListener('click', function (event) {
-    menu.classList.toggle("nav-page__list--hidden");
+    menu.classList.toggle("menu-layout__list--hidden");
+    menu.classList.toggle("menu-layout__list--animation");
 });
